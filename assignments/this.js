c4840ad8e6ff0968216fee17160c9e905e0573d9
LyfeTyme <48641165+LyfeTyme@users.noreply.github.com>
2019-05-29 10:02:03 -0700

    this.js first 2 challenges done

--- assignments/this.js
+++ assignments/this.js
@@ -12,11 +12,37 @@
 // Principle 1
 
 // code example for Window Binding
+function test(name){
+    return `Hello ${this.name}`;
+}
+
+console.log(test("Cody"))
 
 // Principle 2
 
 // code example for Implicit Binding
+const Zebra = {
+    name: "Zebran",
+    age: 34,
+    food: "hay",
+    run: function(){
+        'strict use';
+        return `${this.name} Loves to run`;
+    }
 
+}
+const Lion = {
+    name: "Mufasa",
+    age: 40,
+    food: "Zebras",
+    eat: function(){
+        'strict use';
+        return `${this.name} eats animals like ${Zebra.name}`;
+    }
+
+}
+console.log(Zebra.run());
+console.log(Lion.eat());
 // Principle 3
 
 // code example for New Binding
